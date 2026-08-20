6d8707e684f14101e7e1dcf1cddce4990042f8b9
Akansha Sisodiya <akanshasinghsisodiya5@gmail.com>
2026-08-20 23:24:44 +0530

    Implement LCM and GCD calculation in lcm_gcd.py

---
+++ lcm_gcd.py
@@ -0,0 +1,9 @@
+class Solution:
+    def lcmAndGcd(self, a : int, b : int) -> List[int]:
+        # code here
+        x, y = a, b
+        while y:
+            x, y = y, x % y
+        gcd = x
+        lcm = (a * b) // gcd 
+        return [lcm, gcd]
