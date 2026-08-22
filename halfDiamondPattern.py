532b69d7bd4042524f414321f00874b6a5416fc9
Akansha Sisodiya <akanshasinghsisodiya5@gmail.com>
2026-08-22 15:23:30 +0530

    Add halfDiamondPattern function
    Implement a function to print a half diamond pattern of asterisks.

---
+++ halfDiamondPattern.py
@@ -0,0 +1,10 @@
+def halfDiamondPattern(n):
+  for i in range(n):
+    for j in range(0, i+1):
+      print("*", end="")
+    print()
+    
+  for i in range(1, n):
+    for j in range(i, n):
+      print("*", end="")
+    print()
