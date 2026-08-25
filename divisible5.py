dbe97c91c90effd6efe7eedd91d0eae5040ab5c1
Akansha Sisodiya <akanshasinghsisodiya5@gmail.com>
2026-08-25 12:32:22 +0530

    Add divisibility check for 5 in divisible5.py

---
+++ divisible5.py
@@ -0,0 +1,8 @@
+def main():
+    N = 155
+
+    # Divisibility Check using Modulo Operator
+    if N % 5 == 0:
+        print(f"{N} is divisible by 5")
+    else:
+        print(f"{N} is not divisible by 5")
