089ada15530a7280b5e8a329761effa3cfb77264
Akansha Sisodiya <akanshasinghsisodiya5@gmail.com>
2026-08-25 12:46:04 +0530

    Refactor sum_of_digits and add new methods
    Refactor sum_of_digits function to use last digit extraction and add recursion and string conversion methods for summing digits.

--- sum_of_digits.py
+++ sum_of_digits.py
@@ -1,9 +1,31 @@
+# Using Digit Extraction
 def sum_of_digits(n):
   total = 0
   while n > 0:
-    digits = n % 10
-    total = total + digits
+
+    # Extract the last digit
+    last = n % 10
+    sum += last
+
+    # Remove the last digit
     n = n // 10
-  return total
+  return last
 
 sum_of_digits(123)
+
+
+## using recursion
+def sumofdigit(n):
+  # base case
+  if n == 0:
+    return 0
+  # recursive case
+  return n % 10 + sumofdigit(n // 10)
+
+## using string conversion
+def sumofdigits(n):
+  s = str(n)
+  sum = 0
+  for ch in s:
+    sum += int(ch)
+  return sum 
