a308b61c52bc731dc13c1b3b7d7b0428de9c191e
Akan467 <Akanshasinghsisodiya5@gmail.com>
2026-09-11 13:28:01 +0530

    perfect_number

--- check_Prime_Number.py
+++ check_Prime_Number.py
@@ -24,3 +24,15 @@ elif num > 1:
         print(num, "is not a prime number")
     else:
         print(num, "is a prime number")
+
+
+n = 5
+if n <= 0:
+    print('not prime')
+else:
+    for i in range(2, n):
+        if n % i == 0:
+            print('not prime')
+            break
+    else:
+        print('prime')
---
+++ perfect_number.py
@@ -0,0 +1,22 @@
+def is_perfect(n):
+    sum = 0
+    for i in range(1, n):
+        if n % i == 0:
+            sum += i
+    return sum == n
+
+print(is_perfect(6))
+
+
+def is_perfect(n):
+    if n <= 1:
+        return False
+    total = 1
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            total += i
+            if i != n // i:
+                total += n // i
+    return total == n
+
+print(is_perfect(15))
